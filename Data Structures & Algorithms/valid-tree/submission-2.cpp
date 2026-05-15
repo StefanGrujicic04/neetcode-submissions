f21c87288d1d52b501b7661e77050da53f7fe758
Stefan Grujicic <126318243+StefanGrujicic04@users.noreply.github.com>
2026-05-15 20:06:23 +0200

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int find(int x,vector<int>& parent){
+        if(parent[x]!=x)
+            parent[x]=find(parent[x],parent);
+        return parent[x];    
+    }
+    bool unite(int x,int y,vector<int>&  parent,int&  components){
+        int px=find(x,parent);
+        int py=find(y,parent);
+        if(px==py) return false;
+        parent[py]=px;
+        components--;
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(n-1!=edges.size()) return false;
+        vector<int> parent(n);
+        int components=n; 
+        iota( parent.begin(),parent.end(),0 ); 
+        for(auto& edge: edges){
+                if (!unite(edge[0], edge[1], parent, components))
+                    return false; 
+        }
+        return true;
+    }
+};
