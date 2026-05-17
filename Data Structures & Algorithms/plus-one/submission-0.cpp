316c014c18f50ab6e29579e4a63ce56ffb143732
Stefan Grujicic <126318243+StefanGrujicic04@users.noreply.github.com>
2026-05-17 18:20:09 +0200

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i=digits.size()-1;
+        for(;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                break;
+            }
+            digits[i]=0;
+        }
+        if(i<0){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
